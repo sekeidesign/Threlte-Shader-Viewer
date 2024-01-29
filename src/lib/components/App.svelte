531e91a8718fd2f0543c4fa910c98b297c227750
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-01-29 11:39:15 -0500

    Remove unused code

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -2,11 +2,9 @@
 	import { Canvas } from '@threlte/core';
 	import Scene from './Scene.svelte';
 	import { Pane } from 'svelte-tweakpane-ui';
-
-	let rotation = [0, 0, 0];
 </script>
 
 <Pane></Pane>
 <Canvas>
-	<Scene {rotation} />
+	<Scene />
 </Canvas>
