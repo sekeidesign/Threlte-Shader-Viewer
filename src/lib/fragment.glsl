3f91e3d991fb29f69625bdd101c597cbd25a83cf
Pg Gonni <59372630+sekeidesign@users.noreply.github.com>
2024-02-17 12:18:28 -0500

    Update template to default to full screen, and add more helpful uniforms

--- src/lib/fragment.glsl
+++ src/lib/fragment.glsl
@@ -2,9 +2,14 @@
 
 uniform float uTime;
 varying vec2 vUv;
+uniform vec2 uMouse;
+uniform vec2 uResolution;
 
 void main(void)
 {
+    // Rename the uniform
+    vec2 uv=vUv;
+    
     vec4 color=vec4(vUv,1.,1.);
     gl_FragColor=color;
 }
